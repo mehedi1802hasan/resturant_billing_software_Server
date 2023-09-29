fa7de4f2be49207a761822a0e18f72b3c58b192c
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-09-29 22:23:18 +0600

    mongodb connected new

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express =require('express');
 const cors = require ('cors');
 const app = express();
 require('dotenv').config()
-const port =process.env.PORT || 5000;
+const port =process.env.PORT ||4000;
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(express.json());
 app.use(cors());
 
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rin8xcl.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b0q7jo7.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,48 +27,49 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-   // await client.connect();
+    // await client.connect();
+
     const menuCollection = client.db("billing_Resturant").collection("menu");
 
 
-  //get the all menu    
-app.get('/menu',async(req,res)=>{
-   const result = await menuCollection.find().toArray();
-   res.send(result);
-})
- // post menu
- app.post ('/menu',async(req,res)=>{
-  const addFood = req.body;
-  const result = await menuCollection.insertOne(addFood);
-  // console.log(result)
-  res.send(result);
- })
-
-//delete menu
- app.delete('/menu/:id',async(req,res)=>{
-  const id = req.params.id;
-      const query = { _id: new ObjectId(id)};
-      const result = await menuCollection.deleteOne(query);
+    //get the all menu    
+  app.get('/menu',async(req,res)=>{
+     const result = await menuCollection.find().toArray();
+     res.send(result);
+  })
+   // post menu
+   app.post ('/menu',async(req,res)=>{
+    const addFood = req.body;
+    const result = await menuCollection.insertOne(addFood);
+    // console.log(result)
+    res.send(result);
+   })
   
-      res.send(result);
- })
-// update menu
-app.put('/menu/:id', async (req, res) => {
-  const id = req.params.id;
-  const body = req.body;
-  const filter = { _id: new ObjectId(id) };
-  const updateDoc = {
-    $set: {
-        name: body.name,
-      price: body.price,
-      quantity: body.quantity,
-      image: body.image,
-    },
-  };
-  const result = await menuCollection.updateOne(filter, updateDoc);
-  console.log(result)
-  res.send(result);
-});
+  //delete menu
+   app.delete('/menu/:id',async(req,res)=>{
+    const id = req.params.id;
+        const query = { _id: new ObjectId(id)};
+        const result = await menuCollection.deleteOne(query);
+    
+        res.send(result);
+   })
+  // update menu
+  app.put('/menu/:id', async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+          name: body.name,
+        price: body.price,
+        quantity: body.quantity,
+        image: body.image,
+      },
+    };
+    const result = await menuCollection.updateOne(filter, updateDoc);
+    console.log(result)
+    res.send(result);
+  });
 
 
     // Send a ping to confirm a successful connection
@@ -72,10 +77,11 @@ app.put('/menu/:id', async (req, res) => {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
- //   await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
 
 
 
